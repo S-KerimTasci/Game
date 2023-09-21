c7e3b9b697d3c5bdadaa6b6ca0ac7597abf183ff
Kerim <kerim.tasci@yahoo.de>
2023-09-21 18:34:51 +0200

    requestAnimationFrame()

--- classes/world.class.js
+++ classes/world.class.js
@@ -6,10 +6,12 @@ class World {
         new Chicken(),
         new Chicken()
     ]
+    canvas;
     ctx;
 
-    constructor(){
+    constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
 
     }
@@ -19,7 +21,17 @@ class World {
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
 
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
+        });
+
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        })
     }
 }
