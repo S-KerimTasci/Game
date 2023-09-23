adf06431c9709a57b38ff0ebcb12792047c2c8db
Kerim <kerim.tasci@yahoo.de>
2023-09-23 10:55:56 +0200

    move cloud & chicken

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,13 +1,20 @@
 class Cloud extends MovableObject{
     x = 0;
     y = 50;
-    width = 720;
+    width = 500;
     
 
     constructor(){
         super().loadIMG('../img/5_background/layers/4_clouds/1.png')
 
-        //this.x = 200 + Math.random() * 500;
+        this.x = 200 + Math.random() * 500;
+
+        this.animate();
     }
 
-}
+
+    animate(){
+        setInterval(() => {
+        this.x -= 0.15;
+    }, 1000 / 60 )
+}}
