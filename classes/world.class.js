454be6eff7a052b605f1d7fc6538387d932214ec
Kerim <kerim.tasci@yahoo.de>
2023-09-24 11:20:01 +0200

    mirror Char & and walk out of canvas

--- classes/world.class.js
+++ classes/world.class.js
@@ -19,6 +19,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -36,6 +37,8 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+        this.ctx.translate(this.camera_x,0)
+
         this.addToMap(this.air)
 
         this.addObjectsToMap(this.backgoundobjects)
@@ -45,6 +48,8 @@ class World {
         this.addToMap(this.cloud)
        
         this.addObjectsToMap(this.enemies)
+
+        this.ctx.translate(-this.camera_x,0)
         
 
         let self = this;
@@ -60,6 +65,19 @@ class World {
     }
 
     addToMap(mo){
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1,1);
+            mo.x = mo.x * -1
+        }
+
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
+
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1
+            this.ctx.restore();
+        }
     }
+
 }
